6d230075893743408aca8141bb4942d7266f41e1
Valmir <111082887+val8558@users.noreply.github.com>
2024-10-25 15:51:07 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Valmir Garcia on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
